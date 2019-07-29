d8689d1f5c30a33f0b5eb038d065d6910a621f9a
qrxt <qrx357@gmail.com>
2019-07-29 14:37:50 +0300

    Иконки и поведение для пунктов меню навигации, анимация кнопки бургер-меню

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,10 +1,7 @@
 const burgerMenuBtn = document.querySelector(".main-nav__menu-btn");
 const siteList = document.querySelector(".site-list");
 
-burgerMenuBtn.addEventListener("click", () => {
-  if(siteList.classList.contains("site-list--opened")) {
-    siteList.classList.remove("site-list--opened");
-  } else {
-    siteList.classList.add("site-list--opened");
-  }
+burgerMenuBtn.addEventListener("click", function () {
+  siteList.classList.toggle("site-list--opened");
+  burgerMenuBtn.classList.toggle("burger-menu--opened");
 });
