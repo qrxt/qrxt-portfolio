68207dc45b83bfe465cf53da6b2760e280dd195f
qrxt <qrx357@gmail.com>
2019-08-01 21:12:49 +0300

    a11y фиксы

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -6,6 +6,12 @@ const burgerMenuButtons = document.querySelectorAll(".site-list__link");
 const toggleBurgerMenu = function () {
   siteList.classList.toggle("site-list--opened");
   burgerMenuToggleButton.classList.toggle("burger-menu--opened");
+
+  if(siteList.classList.contains("site-list--opened")) {
+    burgerMenuToggleButton.setAttribute("aria-label", "Закрыть главное меню");
+  } else {
+    burgerMenuToggleButton.setAttribute("aria-label", "Открыть главное меню");
+  }
 };
 
 burgerMenuToggleButton.addEventListener("click", function () {
