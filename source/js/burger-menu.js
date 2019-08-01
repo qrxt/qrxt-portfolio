1206de652f90e035fefc6c6be5c0f0351b502685
qrxt <qrx357@gmail.com>
2019-08-01 23:30:16 +0300

    Изменены мобильные версии превью для работ, добавлены превью работ для планшета

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,25 +1,34 @@
+const ESCAPE_KEYCODE = 27;
+
 const burgerMenuToggleButton = document.querySelector(".main-nav__menu-btn");
 const siteList = document.querySelector(".site-list");
 
-const burgerMenuButtons = document.querySelectorAll(".site-list__link");
+const closeBurgerMenu = function () {
+  if (siteList.classList.contains("site-list--opened")) {
+    siteList.classList.remove("site-list--opened");
+  }
+};
+
+const onEscapeKeydown = function (evt) {
+  if (evt.keyCode === ESCAPE_KEYCODE) {
+    closeBurgerMenu();
+    burgerMenuToggleButton.classList.toggle("burger-menu--opened");
+  }
+};
 
 const toggleBurgerMenu = function () {
   siteList.classList.toggle("site-list--opened");
   burgerMenuToggleButton.classList.toggle("burger-menu--opened");
 
-  if(siteList.classList.contains("site-list--opened")) {
+  if (siteList.classList.contains("site-list--opened")) {
+    document.addEventListener("keydown", onEscapeKeydown);
     burgerMenuToggleButton.setAttribute("aria-label", "Закрыть главное меню");
   } else {
+    document.removeEventListener("keydown", onEscapeKeydown);
     burgerMenuToggleButton.setAttribute("aria-label", "Открыть главное меню");
   }
 };
 
 burgerMenuToggleButton.addEventListener("click", function () {
   toggleBurgerMenu();
-});
-
-burgerMenuButtons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    toggleBurgerMenu();
-  });
 });
