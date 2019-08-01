ad22d94d393265af7a4a42f6adbee4fc0167f05b
qrxt <qrx357@gmail.com>
2019-08-01 18:48:53 +0300

    Новые карточки портфолио, фиксы

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,7 +1,19 @@
-const burgerMenuBtn = document.querySelector(".main-nav__menu-btn");
+const burgerMenuToggleButton = document.querySelector(".main-nav__menu-btn");
 const siteList = document.querySelector(".site-list");
 
-burgerMenuBtn.addEventListener("click", function () {
+const burgerMenuButtons = document.querySelectorAll(".site-list__link");
+
+const toggleBurgerMenu = function () {
   siteList.classList.toggle("site-list--opened");
-  burgerMenuBtn.classList.toggle("burger-menu--opened");
+  burgerMenuToggleButton.classList.toggle("burger-menu--opened");
+};
+
+burgerMenuToggleButton.addEventListener("click", function () {
+  toggleBurgerMenu();
+});
+
+burgerMenuButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    toggleBurgerMenu();
+  });
 });
